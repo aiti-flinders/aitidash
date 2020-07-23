5bf4b040e2fbb148d6b3113a37d321d48eeef106
Hamish Gamble <gamb0043@flinders.edu.au>
2020-07-23 16:00:53 +0930

    enable downloading based on setView

--- modules/labour_market/tab_5_small_area_maps.R
+++ modules/labour_market/tab_5_small_area_maps.R
@@ -14,23 +14,38 @@ labourMarketSmallAreaUI <- function(id, data) {
                    selected = max(data$date)
                  )
              )
-            )
+            ),
+           fluidRow(
+             box(width = 12, status = "info", title = "Downloads", solidHeader = FALSE,
+                 downloadButton(
+                   outputId = ns("download_plot"),
+                   label = "Click here to download the chart as a .png",
+                   class = 'download-button'
+                 )
+           )
+           ),
+           fluidRow(
+             verbatimTextOutput(outputId = ns("text"))
+           )
   )
 }
 
 labourMarketSmallArea <- function(input, output, session, data, region) {  
+
   
   create_data <- reactive({
+    
+    if (region() == "Australia") {
     df <- data %>%
       filter(indicator == "Smoothed unemployment rate (%)",
              date == input$date) 
-    
-    if (region() != "Australia") {
-      df <- df %>%
-        filter(state_name_2016 == region())
-    }  
-    
-    })
+    } else {
+      df <- data %>%
+        filter(indicator == "Smoothed unemployment rate (%)",
+               date == input$date,
+               state_name_2016 == region())
+    }
+  })
   
   create_plot <- reactive({
     
@@ -64,6 +79,31 @@ labourMarketSmallArea <- function(input, output, session, data, region) {
   output$map <- renderLeaflet({
     create_plot()
   })
+  
+  user_map <- reactive({
+    create_plot() %>%
+      setView(lng = input$map_center$lng,
+              lat = input$map_center$lat,
+              zoom = input$map_zoom)
+  })
+  
+  output$download_plot <- downloadHandler(
+    filename = function(){
+      paste(region(), "-map.png", sep = '')
+    },
+    content = function(file) {
+      mapview::mapshot(user_map(), file = file, cliprect = "viewport", selfcontained = FALSE)
+    }
+  )
+  
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste(input$indicator, "-data.csv", sep = '')
+    },
+    content = function(file) {
+      write.csv(create_data(), file, row.names = FALSE)
+    }
+  )
   
 
 
