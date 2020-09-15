4646c891f4cdb08eccaee8454b7972be696283ea
Hamish Gamble <gamb0043@flinders.edu.au>
2020-09-15 10:44:25 +0930

    finalise action for release

--- R/dashboard.R
+++ R/dashboard.R
@@ -2,18 +2,16 @@ dashboardUI <- function(id, data) {
   ns <- NS(id)
   
   lf_next_release <- abs_next_release("6202.0")
-  lf_release_date <- abs_current_release("6202.0")
   industry_next_release <- abs_next_release("6291.0.55.003")
-  industry_release_date <- abs_current_release("6291.0.55.003")
-  
+
   fluidPage(
   h2("Employment Insights"),
   h2(textOutput("region_selected")),
   p(paste0("This data is current as of ", release(labour_force, "month"), " ", release(labour_force, "year"),".")),
   p(paste0("Data for ", 
-           lf_next_release, 
+           release(labour_force, "month", plus = 1), 
            " will be available on ", 
-           weekdays(lf_release_date), ", the ", day(lf_release_date), "th of ", month(lf_release_date, abbr = F, label = T), ".")),
+           weekdays(lf_next_release), ", the ", day(lf_next_release), "th of ", month(lf_next_release, abbr = F, label = T), ".")),
   fluidRow( 
     boxesUI('unemployment_rate'),
     boxesUI('unemployed')
@@ -46,9 +44,9 @@ dashboardUI <- function(id, data) {
   ),
   p(paste0("This data is current as at: ", release(employment_by_industry, "month"), " ", release(employment_by_industry, "year"))),
   p(paste0("Data for ", 
-           industry_next_release, 
+           release(employment_by_industry, "month"), 
            " will be available on ", 
-           weekdays(industry_release_date), ", the ", day(industry_release_date), "th of ", month(industry_release_date, abbr = F, label = T), ".")),
+           weekdays(industry_next_release), ", the ", day(industry_next_release), "th of ", month(industry_next_release, abbr = F, label = T), ".")),
   
   fluidRow(
     boxesAltUI("industry_total"),
