317374132dacf53f38fdf8267a842cd6ec887b35
Hamish Gamble <hamish.gamble@gmail.com>
2022-02-02 13:08:51 +1030

    Adds plotly auth

--- R/dashboard_app.R
+++ R/dashboard_app.R
@@ -30,9 +30,9 @@
 
 
 #### Preamble ####
-# Plotly Setup
-# Sys.setenv("plotly_username" = "hamgamb")
-# Sys.setenv("plotly_api_key" = 'SDYMDyK3YM0eZrTNpyoa')
+Plotly Setup
+Sys.setenv("plotly_username" = "hamgamb")
+Sys.setenv("plotly_api_key" = 'SDYMDyK3YM0eZrTNpyoa')
 
 # # phantom_js install
 # if (is.null(suppressMessages(webshot:::find_phantom()))) { webshot::install_phantomjs() }
