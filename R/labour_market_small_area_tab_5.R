e5cbef17a81b38c282ba3bfe992cd9d49368aabf
dnicoll70 <92497735+dnicoll70@users.noreply.github.com>
2024-02-16 10:57:31 +1030

    Attempt to fix issues since Load_all command updated.

--- R/labour_market_small_area_tab_5.R
+++ R/labour_market_small_area_tab_5.R
@@ -46,7 +46,8 @@ labourMarketSmallAreaServer <- function(id, data, region) {
       filter(indicator == input$indicator,
              date == as.Date(as.yearqtr(input$date)) + months(2)) %>%
       mutate(value_label = ifelse(grepl("%", indicator), as_percent(value), as_comma(value))) %>%
-      left_join(strayr::read_absmaps("sa22016")) %>%
+      #left_join(strayr::read_absmaps("sa22016")) %>%
+      left_join(strayr::read_absmap("sa22016")) %>% # suspected typo in above map vs maps
       st_as_sf()
     } else {
       df <- data %>%
@@ -54,7 +55,8 @@ labourMarketSmallAreaServer <- function(id, data, region) {
                date == as.Date(as.yearqtr(input$date)) + months(2),
                state_name_2016 == region()) %>%
         mutate(value_label = ifelse(grepl("%", indicator), as_percent(value), as_comma(value))) %>%
-        left_join(strayr::read_absmaps("sa22016")) %>%
+        #left_join(strayr::read_absmaps("sa22016")) %>%
+        left_join(strayr::read_absmaps("sa22016")) %>% # suspected typo in above map vs maps
         st_as_sf()
     }
   })
--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+#pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+pkgload::load_all(export_all = TRUE, helpers = FALSE, attach_testthat = FALSE)
 aiti_dashboard() # add parameters here (if any)
  
